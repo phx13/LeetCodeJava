6539e8266acc6848fedf1c870e959b0a96130f4c
phx <945871257@qq.com>
2021-04-28 10:21:23 +0100

    create q1370

--- src/sort/Q1370_Increasing_Decreasing_String.java
+++ src/sort/Q1370_Increasing_Decreasing_String.java
@@ -1,5 +1,11 @@
 package sort;
 
 public class Q1370_Increasing_Decreasing_String {
-
+	public static void main(String[] args) {
+		String s = "aaaabbbbcccc";
+		sortString(s);
+	}
+	public static String sortString(String s) {
+		return s;
+    }
 }
